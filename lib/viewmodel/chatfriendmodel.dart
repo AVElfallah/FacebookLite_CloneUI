aad4fbfd4573e4dd819593eae07ad96d96dca77d
abdulrhman elfallah <abdohegag@gmail.com>
2022-08-23 12:42:28 +0200

    delete junk files

--- lib/viewmodel/chatfriendmodel.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class FBChatFriend {
-  final bool isOnline;
-  final String accountName;
-  final ImageProvider accountPhoto;
-
-  FBChatFriend(this.accountName, this.accountPhoto, this.isOnline);
-}
